1281d6214230007fe67cd806eb67f380d3bf9179
Maxime Peschard <maxime.peschard@gmail.com>
2017-12-17 18:24:41 +0100

    Add string utilities

--- common/io.ml
+++ common/io.ml
@@ -6,3 +6,10 @@ let read_lines filename =
     | line -> file_to_lines (line :: acc) ic
     | exception End_of_file -> close_in ic; List.rev acc
   in file_to_lines [] ic
+
+(* Return the string representation of a list *)
+let string_of_list string_of_elt delim l =
+  String.concat delim (List.map string_of_elt l)
+
+(* Get a string from a list of characters *)
+let string_of_chars l = string_of_list (fun c -> Printf.sprintf "%c" c) "" l
